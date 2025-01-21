49b06d1bad2760f5e477648ae8170ceac4b6c233
Leonardo-Corazzini <leocora10@gmail.com>
2025-01-21 17:43:47 +0100

    add routes and pages

---
+++ client/src/pages/InsertPropertyPage.jsx
@@ -0,0 +1,5 @@
+export default function InsertPropertyPage() {
+    return (
+        <div>InsertPropertyPage</div>
+    )
+}
---
+++ client/src/pages/PropertyDetails.jsx
@@ -0,0 +1,5 @@
+export default function PropertyDetails() {
+    return (
+        <div>dettaglio proprery</div>
+    )
+}
