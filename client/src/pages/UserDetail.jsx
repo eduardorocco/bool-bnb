e09442e0bb758f9a78941d9f60ed8c6de2e42ab7
AndreaPotenza <andreapoten3@gmail.com>
2025-01-24 17:25:42 +0100

    add proprietà a userDetails

--- client/src/pages/UserDetail.jsx
+++ client/src/pages/UserDetail.jsx
@@ -1,13 +1,40 @@
-import { useState, useContext } from "react"
-import { Link } from "react-router"
+import { useState, useContext, useEffect } from "react"
+import { Link, useParams } from "react-router"
 import GlobalContext from "../context/GlobalContext"
+import axios from "axios"
 export default function UserDetail() {
 
-    const { user } = useContext(GlobalContext)
-    const { id } = user
+    const { user, setUser, API_URL } = useContext(GlobalContext)
+    // const { id } = useParams()
+    //const [user, setUser] = useState({})
+
+
+
+    // function fetchUser() {
+    //     axios.get(`${API_URL}users/${id}`)
+    //         .then(res => {
+    //             setUser(res.data[0])
+    //         }).catch(err => {
+    //             console.err(err)
+    //         })
+    // }
+
+    // useEffect(() => {
+    //     fetchUser()
+    // }, [id])
+
+
+    const { name, surname, number, email, username } = user
+
     return (
         <>
-            <span>user detail</span>
+            <div>
+                <p>Nome:{name}</p>
+                <p>Cognome:{surname}</p>
+                <p>Numero:{number}</p>
+                <p>email:{email}</p>
+                <p>Username:{username}</p>
+            </div>
             <Link className="btn btn-danger" to={'properties'} >insert properties</Link>
         </>
 
