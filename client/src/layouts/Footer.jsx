558c6f144d78410aa905160439dfbdb0416889ed
eduardorocco <eduardo_rocco@hotmail.it>
2025-01-27 16:45:54 +0100

    feat: add Footer component with styling and links to contributors

--- client/src/layouts/Footer.jsx
+++ client/src/layouts/Footer.jsx
@@ -1,5 +1,19 @@
+import style from '../assets/modules/Footer.module.css'
+import logo from '../assets/logo-gallery/turquoise-circle-text.png'
+import {Link} from 'react-router'
 export default function Footer() {
     return (
-        <div className="container">Hfoooter</div>
+        <div className={`${style.footer} container-fluid`}>
+            <figure className={style.logo}>
+                <img src={logo}/>
+            </figure>
+            <div className={style.links}>
+                <Link className={style.a}  to="https://github.com/Leonardo-Corazzini" target="_blank">Leonardo Corazzini</Link>
+                <Link className={style.a}  to="https://github.com/GiuliaMoiraghi10" target="_blank">Giulia Moiraghi</Link>
+                <Link className={style.a}  to="https://github.com/AndreaPotenza3" target="_blank">Andrea Potenza</Link>
+                <Link className={style.a}  to="https://github.com/eduardorocco" target="_blank">Eduardo Rocco</Link>
+                <Link className={style.a}  to="https://github.com/VitaFrancesco" target="_blank">Francesco Vita</Link>
+            </div>
+        </div>
     )
 }
